09360e4d5c14d160368516c2022f95eadd8ba17a
Phil Thompson <phil@electricvisions.com>
2015-09-24 17:43:57 +0100

    correctly parse date

--- lib/contract_ltd/dividend.rb
+++ lib/contract_ltd/dividend.rb
@@ -6,13 +6,13 @@ module ContractLtd
       @name = 'dividend'
       @type = ARGV[0] == 'final' ? 'Final' : 'Interim'
       @date_paid = Date.parse(ARGV[1])
-      @tax_year_end = Date.parse(ARGV[2], 4, 5).to_s(:long)
+      @tax_year_end = Date.new(ARGV[2].to_i, 4, 5).to_s(:long)
       @shares = 100
       @net = ARGV[3].to_f
       @tax_credit = @net * 0.111111
       @gross = @net + @tax_credit
       @amount = @net / @shares
-      @company_year_end = Date.parse(ARGV[4], 2, 28).to_s(:long)
+      @company_year_end = Date.new(ARGV[4].to_i, 2, 28).to_s(:long)
     end
 
     def date_paid(format = :long)
